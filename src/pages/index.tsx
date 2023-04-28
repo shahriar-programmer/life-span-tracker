dca2dd0535970ca439ad655c423275a696373f68
shahriar-programmer <shovonshahriar01@gmail.com>
2023-04-28 09:51:14 +0600

    Added meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,6 +21,17 @@ export default function Home() {
     <main className="w-full h-screen p-4 bg-gray-900 text-white/70">
       <Head>
         <title>Life Span Progress Tracker</title>
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="Visualize your Life Span Progress with our interactive graph tracker. Simply input your date of birth and see how many weeks you've already lived and how many you have left until the age of 72. Stay motivated and track your progress towards your life goals with ease."
+        />
+        <meta
+          name="keywords"
+          content="Life span, progress, tracker, shahriar ahmed shovon"
+        />
+        <meta name="author" content="Shahriar Ahmed Shovon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       {inputModal && (
         <InputModal setDOB={setDOB} setInputModal={setInputModal} />
